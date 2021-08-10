286d1697b3698ca7d2abe5ed881fa652ef6cf058
czm@local.lan <915734072@qq.com>
2021-08-10 09:15:49 +0800

    sync

--- 02-Linux系统发送告警脚本.sh
+++ 02-Linux系统发送告警脚本.sh
@@ -5,3 +5,6 @@ sed -i '$a set smtp-auth-user=zhimengcai@yeah.net smtp-auth-password=AYFWPSOKGYV
 sed -i '$a set smtp-auth=login' /etc/mail.rc
 # $a和 a 有不同，分别是在文件末尾添加，zai每一行的末尾添加。
 
+#1.需要安装mailx组件，可以rpm安装
+#2.随便注册一个邮箱，登录邮箱，开启stmp功能，linux能登录stmp服务器，利用该邮箱发送邮件。
+#3.修改Linux上的mail.rc文件添加密钥、启动配置。
